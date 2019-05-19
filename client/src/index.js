35f1c0feedc7918a1b93d10fc35740d3ad72be0a
Artur Sousa <arturfelipe.sousa@gmail.com>
2019-05-19 13:44:39 -0300

    Setup the apollo-client

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import client from './client';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
